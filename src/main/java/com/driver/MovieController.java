3bffbfb49aa93c4dea4b9e477c5e51963b5248d9
kumaresh1597 <kumaresh.1597@gmail.com>
2023-05-31 17:40:12 +0530

    fourth

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("movies")
 public class MovieController {
     @Autowired
     private MovieService movieService;
